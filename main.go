f5deca8742cdd9ec85023514e5146e7bcbfc7ee1
slotti <samlotti@fastmail.com>
2023-02-24 15:36:49 -0500

    added banner

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"relKV/backup"
 	"relKV/cmd"
 )
 
 const Version = "0.1.0"
 
+const Banner = `
+██████╗ ███████╗██╗     ██╗  ██╗██╗   ██╗
+██╔══██╗██╔════╝██║     ██║ ██╔╝██║   ██║
+██████╔╝█████╗  ██║     █████╔╝ ██║   ██║
+██╔══██╗██╔══╝  ██║     ██╔═██╗ ╚██╗ ██╔╝
+██║  ██║███████╗███████╗██║  ██╗ ╚████╔╝ 
+╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  
+                                         
+`
+
 func main() {
+	fmt.Println(Banner)
 	readyChannel := make(chan *cmd.BucketsDb)
 	go cmd.BootServer(Version, readyChannel)
 	bk := <-readyChannel
